f0fe406eaa78a0b1d584ee5c8e167cc653805103
ImRishabhRawat <rishabhrawat757@gmail.com>
2024-01-24 18:33:59 +0530

    update localhost to virtual host

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -26,7 +26,7 @@ const CreatePost = () => {
     if (form.title && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://open-book-9xyb.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -52,7 +52,7 @@ const CreatePost = () => {
       console.log(form.title);
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/book', {
+        const response = await fetch('https://open-book-9xyb.onrender.com/api/v1/book', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
